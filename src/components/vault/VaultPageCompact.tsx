dfaf0195886d7705764ee78ae8fd0e67a3dbb6ee
heyjinjung <heyjinjung@users.noreply.github.com>
2025-12-31 17:00:01 +0900

    feat: add `VaultPageCompact` component for a new, streamlined vault page experience.

--- src/components/vault/VaultPageCompact.tsx
+++ src/components/vault/VaultPageCompact.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo, useEffect } from "react";
+import React, { useState, useMemo } from "react";
 import clsx from "clsx";
 import { useQuery } from "@tanstack/react-query";
 import { getVaultStatus } from "../../api/vaultApi";
